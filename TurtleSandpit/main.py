91eada5683c6a6c72c3a2f0f99cf42ab6bc3cea4
Geoffysicist <66104692+Geoffysicist@users.noreply.github.com>
2021-06-04 11:45:45 +1000

    Add files via upload

---
+++ TurtleSandpit/main.py
@@ -0,0 +1,11 @@
+""" Simple script introducing python using turtle.
+
+A script to introduce python 3.x
+Introduces basic concepts using the turtle module
+"""
+
+import turtle as t
+
+donatello = t.Turtle()
+
+donatello.forward(100)
